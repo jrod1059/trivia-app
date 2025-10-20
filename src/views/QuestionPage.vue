1ccccf20833561539dcbde54de12989c84e21a07
Jonathan Rodriguez <rodriguez.jonathan77@yahoo.com>
2025-10-20 17:33:12 -0500

    difficulty added.

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,6 +3,8 @@ import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
 import BaseTitle from '@/components/BaseTitle.vue';
+import DifficultyChip from '@/components/DifficultyChip.vue';
+
 
 const api = useAPI()
 const question = ref(null)
@@ -66,6 +68,9 @@ class="bg-blue-700 flex items-center justify-center text-4xl rounded-lg text-whi
 </div>
 
 </div>
+<DifficultyChip :difficulty="question.difficulty"></DifficultyChip>
+
+
 </div>
 
 <div v-else class="">
